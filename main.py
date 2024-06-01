671fec82e98a548d6e7aceb458b52a113ce4b7e6
A. Sam Richard <115559742+Sam-Richard-007@users.noreply.github.com>
2024-06-01 17:27:40 +0530

    Update main.py
    final

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pyrogram
 from pyrogram import Client, filters
-from pyrogram.errors import UserAlreadyParticipant, InviteHashExpired, UsernameNotOccupied, FloodWait
+from pyrogram.errors import UserAlreadyParticipant, InviteHashExpired, UsernameNotOccupied, FloodWait, RPCError
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 import time
@@ -17,18 +17,18 @@ bot = Client("mybot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 ss = environ.get("STRING", "BQB9n4DQRxBDwPHDH4knrS1kdslx8h83a_ywix6pem8saJLrVekogsR0VsIZseR5ePiPnrNa8bwPLs5GPFNqvSNapiBbYt-Psd3S_gemqub0OWNN9xxXAFAMfPd8nw1czBDce-zcJGSaJM47p1guNt5PBUY-Yaj1FRPzP5AwjFLD8_MZ2H2ZUorNZrC7AUMq6NE4lke8InMQnMpgws2JLpx1_x5XBPqKe9Oco740nJXsA3k1kJrkoqhiDnsr9zq9bxVHWpJRZgOqRhrVv0m6nh3SvODOB_xaM_5Wy2ZPcUWNt8q86ojpRIw62Rj-cvYLgp64yDIcMQK7CO3MwfLUdsNgAAAAAZNijZIA")
 
 if ss is not None:
-    acc = Client("myacc", api_id=api_id, api_hash=api_hash, session_string=ss)
-    acc.start()
-else:
-    acc = None
+	acc = Client("myacc" ,api_id=api_id, api_hash=api_hash, session_string=ss)
+	acc.start()
+else: acc = None
+
+# new channel ID
+new_channel_id = int(environ.get("NEW_CHANNEL_ID", "-1002168376712"))
 
 # download status
 def downstatus(statusfile, message):
-    while True:
-        if os.path.exists(statusfile):
-            break
+    while not os.path.exists(statusfile):
+        time.sleep(3)
 
-    time.sleep(3)
     while os.path.exists(statusfile):
         with open(statusfile, "r") as downread:
             txt = downread.read()
@@ -40,11 +40,9 @@ def downstatus(statusfile, message):
 
 # upload status
 def upstatus(statusfile, message):
-    while True:
-        if os.path.exists(statusfile):
-            break
+    while not os.path.exists(statusfile):
+        time.sleep(3)
 
-    time.sleep(3)
     while os.path.exists(statusfile):
         with open(statusfile, "r") as upread:
             txt = upread.read()
@@ -63,29 +61,7 @@ def progress(current, total, message, type):
 @bot.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
     bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**, **I am Save Restricted Bot, I can send you restricted content by its post link__**\n\n{USAGE}",
-    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🌐 Update Channel", url="https://t.me/VJ_Botz")]]), reply_to_message_id=message.id)
-
-# Add channel joining functionality
-@bot.on_message(filters.command(["joinchannel"]))
-def join_channel(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-    if len(message.command) != 2:
-        bot.send_message(message.chat.id, "**Usage:** /joinchannel <invite link>", reply_to_message_id=message.id)
-        return
-
-    invite_link = message.command[1]
-    if acc is None:
-        bot.send_message(message.chat.id, f"**String Session is not Set**", reply_to_message_id=message.id)
-        return
-
-    try:
-        acc.join_chat(invite_link)
-        bot.send_message(message.chat.id, "**Channel Joined**", reply_to_message_id=message.id)
-    except UserAlreadyParticipant:
-        bot.send_message(message.chat.id, "**Already a member of the channel**", reply_to_message_id=message.id)
-    except InviteHashExpired:
-        bot.send_message(message.chat.id, "**Invalid Link**", reply_to_message_id=message.id)
-    except Exception as e:
-        bot.send_message(message.chat.id, f"**Error** : __{e}__", reply_to_message_id=message.id)
+                     reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🌐 Update Channel", url="https://t.me/VJ_Botz")]]), reply_to_message_id=message.id)
 
 @bot.on_message(filters.text)
 def save(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
@@ -98,14 +74,16 @@ def save(client: pyrogram.client.Client, message: pyrogram.types.messages_and_me
             return
 
         try:
-            acc.join_chat(message.text)
+            try:
+                acc.join_chat(message.text)
+            except Exception as e:
+                bot.send_message(message.chat.id, f"**Error** : __{e}__", reply_to_message_id=message.id)
+                return
             bot.send_message(message.chat.id, "**Chat Joined**", reply_to_message_id=message.id)
         except UserAlreadyParticipant:
             bot.send_message(message.chat.id, "**Chat already Joined**", reply_to_message_id=message.id)
         except InviteHashExpired:
             bot.send_message(message.chat.id, "**Invalid Link**", reply_to_message_id=message.id)
-        except Exception as e:
-            bot.send_message(message.chat.id, f"**Error** : __{e}__", reply_to_message_id=message.id)
 
     # getting message
     elif "https://t.me/" in message.text:
@@ -121,25 +99,29 @@ def save(client: pyrogram.client.Client, message: pyrogram.types.messages_and_me
             # private
             if "https://t.me/c/" in message.text:
                 chatid = int("-100" + datas[4])
+
                 if acc is None:
                     bot.send_message(message.chat.id, f"**String Session is not Set**", reply_to_message_id=message.id)
                     return
-                handle_private(message, chatid, msgid, forward_to_channel=True)
 
+                handle_private(message, chatid, msgid)
+            
             # bot
             elif "https://t.me/b/" in message.text:
                 username = datas[4]
+
                 if acc is None:
                     bot.send_message(message.chat.id, f"**String Session is not Set**", reply_to_message_id=message.id)
                     return
                 try:
-                    handle_private(message, username, msgid, forward_to_channel=True)
+                    handle_private(message, username, msgid)
                 except Exception as e:
                     bot.send_message(message.chat.id, f"**Error** : __{e}__", reply_to_message_id=message.id)
 
             # public
             else:
                 username = datas[3]
+
                 try:
                     msg = bot.get_messages(username, msgid)
                 except UsernameNotOccupied:
@@ -147,99 +129,98 @@ def save(client: pyrogram.client.Client, message: pyrogram.types.messages_and_me
                     return
 
                 try:
-                    bot.copy_message(message.chat.id, msg.chat.id, msg.id, reply_to_message_id=message.id)
-                except:
+                    bot.copy_message(new_channel_id, msg.chat.id, msg.id)
+                except Exception as e:
                     if acc is None:
                         bot.send_message(message.chat.id, f"**String Session is not Set**", reply_to_message_id=message.id)
                         return
                     try:
-                        handle_private(message, username, msgid, forward_to_channel=True)
+                        handle_private(message, username, msgid)
                     except Exception as e:
                         bot.send_message(message.chat.id, f"**Error** : __{e}__", reply_to_message_id=message.id)
 
             # wait time
-            time.sleep(3)
+            time.sleep(1)
 
 # handle private
-def handle_private(message: pyrogram.types.messages_and_media.message.Message, chatid: int, msgid: int, forward_to_channel=False):
+def handle_private(message: pyrogram.types.messages_and_media.message.Message, chatid: int, msgid: int):
     try:
-        msg: pyrogram.types.messages_and_media.message.Message = acc.get_messages(chatid, msgid)
-        msg_type = get_message_type(msg)
-
-        if "Text" == msg_type:
-            bot.send_message(message.chat.id, msg.text, entities=msg.entities, reply_to_message_id=message.id)
-            return
-
-        smsg = bot.send_message(message.chat.id, '__Downloading__', reply_to_message_id=message.id)
-        dosta = threading.Thread(target=lambda: downstatus(f'{message.id}downstatus.txt', smsg), daemon=True)
-        dosta.start()
-        file = acc.download_media(msg, progress=progress, progress_args=[message, "down"])
-        os.remove(f'{message.id}downstatus.txt')
-
-        upsta = threading.Thread(target=lambda: upstatus(f'{message.id}upstatus.txt', smsg), daemon=True)
-        upsta.start()
-
-        caption = msg.caption if msg.caption else ""
-        if "Document" == msg_type:
-            if forward_to_channel:
-                bot.send_document(DEST_CHANNEL_ID, file, caption=caption, progress=progress, progress_args=[message, "up"])
-            else:
-                bot.send_document(message.chat.id, file, caption=caption, reply_to_message_id=message.id, progress=progress, progress_args=[message, "up"])
-
-        elif "Video" == msg_type:
-            if forward_to_channel:
-                bot.send_video(DEST_CHANNEL_ID, file, caption=caption, progress=progress, progress_args=[message, "up"])
-            else:
-                bot.send_video(message.chat.id, file, caption=caption, reply_to_message_id=message.id, progress=progress, progress_args=[message, "up"])
-
-        elif "Animation" == msg_type:
-            if forward_to_channel:
-                bot.send_animation(DEST_CHANNEL_ID, file, caption=caption, progress=progress, progress_args=[message, "up"])
-            else:
-                bot.send_animation(message.chat.id, file, caption=caption, reply_to_message_id=message.id, progress=progress, progress_args=[message, "up"])
-
-        elif "Sticker" == msg_type:
-            if forward_to_channel:
-                bot.send_sticker(DEST_CHANNEL_ID, file, progress=progress, progress_args=[message, "up"])
-            else:
-                bot.send_sticker(message.chat.id, file, reply_to_message_id=message.id, progress=progress, progress_args=[message, "up"])
-
-        elif "Voice" == msg_type:
-            if forward_to_channel:
-                bot.send_voice(DEST_CHANNEL_ID, file, caption=caption, progress=progress, progress_args=[message, "up"])
-            else:
-                bot.send_voice(message.chat.id, file, caption=caption, reply_to_message_id=message.id, progress=progress, progress_args=[message, "up"])
-
-        elif "Audio" == msg_type:
-            if forward_to_channel:
-                bot.send_audio(DEST_CHANNEL_ID, file, caption=caption, progress=progress, progress_args=[message, "up"])
-            else:
-                bot.send_audio(message.chat.id, file, caption=caption, reply_to_message_id=message.id, progress=progress, progress_args=[message, "up"])
-
-        elif "Photo" == msg_type:
-            if forward_to_channel:
-                bot.send_photo(DEST_CHANNEL_ID, file, caption=caption, progress=progress, progress_args=[message, "up"])
-            else:
-                bot.send_photo(message.chat.id, file, caption=caption, reply_to_message_id=message.id, progress=progress, progress_args=[message, "up"])
-
-        try:
-            os.remove(f'{message.id}upstatus.txt')
-        except FileNotFoundError:
-            pass
-        os.remove(file)
-        bot.delete_messages(message.chat.id, smsg.id)
+        msg = acc.get_messages(chatid, msgid)
     except FloodWait as e:
-        wait_time = e.x
-        bot.send_message(message.chat.id, f"Flood wait error. Waiting for {wait_time} seconds.")
-        time.sleep(wait_time)
-        handle_private(message, chatid, msgid, forward_to_channel)
-    except FileNotFoundError as e:
-        bot.send_message(message.chat.id, f"File not found error: {e}")
-    except Exception as e:
-        bot.send_message(message.chat.id, f"An unexpected error occurred: {e}")
+        time.sleep(e.x)
+        msg = acc.get_messages(chatid, msgid)
+    except RPCError as e:
+        bot.send_message(message.chat.id, f"**RPC Error** : __{e}__", reply_to_message_id=message.id)
+        return
 
-# get message type
-def get_message_type(msg: pyrogram.types.messages_and_media.message.Message) -> str:
+    msg_type = get_message_type(msg)
+
+    if "Text" == msg_type:
+        bot.send_message(new_channel_id, msg.text, entities=msg.entities)
+        return
+
+    smsg = bot.send_message(message.chat.id, '__Downloading__', reply_to_message_id=message.id)
+    dosta = threading.Thread(target=lambda: downstatus(f'{message.id}downstatus.txt', smsg), daemon=True)
+    dosta.start()
+    try:
+        file = acc.download_media(msg, progress=progress, progress_args=[message, "down"])
+    except FloodWait as e:
+        time.sleep(e.x)
+        file = acc.download_media(msg, progress=progress, progress_args=[message, "down"])
+    os.remove(f'{message.id}downstatus.txt')
+
+    upsta = threading.Thread(target=lambda: upstatus(f'{message.id}upstatus.txt', smsg), daemon=True)
+    upsta.start()
+
+    if "Document" == msg_type:
+        try:
+            thumb = acc.download_media(msg.document.thumbs[0].file_id)
+        except:
+            thumb = None
+
+        bot.send_document(new_channel_id, file, thumb=thumb, caption=msg.caption, caption_entities=msg.caption_entities, progress=progress, progress_args=[message, "up"])
+        if thumb is not None:
+            os.remove(thumb)
+
+    elif "Video" == msg_type:
+        try:
+            thumb = acc.download_media(msg.video.thumbs[0].file_id)
+        except:
+            thumb = None
+
+        bot.send_video(new_channel_id, file, duration=msg.video.duration, width=msg.video.width, height=msg.video.height, thumb=thumb, caption=msg.caption, caption_entities=msg.caption_entities, progress=progress, progress_args=[message, "up"])
+        if thumb is not None:
+            os.remove(thumb)
+
+    elif "Animation" == msg_type:
+        bot.send_animation(new_channel_id, file)
+
+    elif "Sticker" == msg_type:
+        bot.send_sticker(new_channel_id, file)
+
+    elif "Voice" == msg_type:
+        bot.send_voice(new_channel_id, file, caption=msg.caption, caption_entities=msg.caption_entities, progress=progress, progress_args=[message, "up"])
+
+    elif "Audio" == msg_type:
+        try:
+            thumb = acc.download_media(msg.audio.thumbs[0].file_id)
+        except:
+            thumb = None
+
+        bot.send_audio(new_channel_id, file, caption=msg.caption, caption_entities=msg.caption_entities, progress=progress, progress_args=[message, "up"])
+        if thumb is not None:
+            os.remove(thumb)
+
+    elif "Photo" == msg_type:
+        bot.send_photo(new_channel_id, file, caption=msg.caption, caption_entities=msg.caption_entities)
+
+    os.remove(file)
+    if os.path.exists(f'{message.id}upstatus.txt'):
+        os.remove(f'{message.id}upstatus.txt')
+    bot.delete_messages(message.chat.id, [smsg.id])
+
+# get the type of message
+def get_message_type(msg: pyrogram.types.messages_and_media.message.Message):
     try:
         msg.document.file_id
         return "Document"
@@ -282,22 +263,39 @@ def get_message_type(msg: pyrogram.types.messages_and_media.message.Message) -> 
     except:
         pass
 
-    return "Text"
+    try:
+        msg.text
+        return "Text"
+    except:
+        pass
 
 USAGE = """**FOR PUBLIC CHATS**
-`https://t.me/username/2` - `https://t.me/username/2-100`
-`https://t.me/c/12345678/2` - `https://t.me/c/12345678/2-100`
 
-**FOR BOTS**
-`https://t.me/b/username/2` - `https://t.me/b/username/2-100`
+**__just send post/s link__**
 
 **FOR PRIVATE CHATS**
-`https://t.me/c/12345678/2` - `https://t.me/c/12345678/2-100`
 
-**JOIN CHAT USING LINK**
-`https://t.me/+/link` - `https://t.me/joinchat/link`
+**__first send invite link of the chat (unnecessary if the account of string session already member of the chat)
+then send post/s link__**
+
+**FOR BOT CHATS**
+
+**__send link with** '/b/', **bot's username and message id, you might want to install some unofficial client to get the id like below__**
+
+https://t.me/b/botusername/4321
+
+
+**MULTI POSTS**
+
+**__send public/private posts link as explained above with format "from - to" to send multiple messages like below__**
+
+https://t.me/xxxx/1001-1010
+
+https://t.me/c/xxxx/101 - 120
+
+
+**__note that space in between doesn't matter__**
 """
 
-DEST_CHANNEL_ID = -1002168376712  # Replace with your channel's ID
-
+# infinite polling
 bot.run()
